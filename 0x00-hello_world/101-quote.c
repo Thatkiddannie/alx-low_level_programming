e0f0657ec081aaeae4c7e65b3b7f9d7ee4e09c5c
Thatkiddannie <thatkiddannie@gmail.com>
2023-09-09 18:17:37 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  main - main function of prgram
+ *  Return: returns 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 65);
+	return (1);
+}
