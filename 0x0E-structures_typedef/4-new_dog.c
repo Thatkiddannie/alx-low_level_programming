bca34cb882ff05604da7b225cc6dc18d28cea6e9
Thatkiddannie <thatkiddannie@gmail.com>
2023-10-11 16:28:10 -0700

    [Fix]

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -79,4 +79,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
 
+	return (dog);
+}
