103592eb5902cfa03906f327f7ccb8ffb98b77ad
Thatkiddannie <thatkiddannie@gmail.com>
2023-10-19 17:44:38 -0700

    [Fix for task 3 and solving adv task 101

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -30,4 +30,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		*head = new;
 		return (new);
+	}
 
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
